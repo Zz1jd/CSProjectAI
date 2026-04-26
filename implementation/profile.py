1fc10413ae5907915d98b3dd201239aacb7545b4
MJWade96 <2958594926@qq.com>
2026-04-26 10:51:50 +0800

    revert(profile): keep direct tensorboard import

--- implementation/profile.py
+++ implementation/profile.py
@@ -8,13 +8,7 @@ from typing import List, Dict
 import logging
 import json
 from implementation import code_manipulation
-
-try:
-    from torch.utils.tensorboard import SummaryWriter
-    _SUMMARY_WRITER_IMPORT_ERROR = None
-except (ImportError, ModuleNotFoundError) as exc:
-    SummaryWriter = None
-    _SUMMARY_WRITER_IMPORT_ERROR = exc
+from torch.utils.tensorboard import SummaryWriter
 
 
 class Profiler:
@@ -46,11 +40,6 @@ class Profiler:
         self._all_sampled_functions: Dict[int, code_manipulation.Function] = {}
 
         if log_dir:
-            if SummaryWriter is None:
-                raise RuntimeError(
-                    "TensorBoard SummaryWriter is unavailable; install tensorboard "
-                    "or run without profiler logging."
-                ) from _SUMMARY_WRITER_IMPORT_ERROR
             self._writer = SummaryWriter(log_dir=log_dir)
 
         self._each_sample_best_program_score = []
